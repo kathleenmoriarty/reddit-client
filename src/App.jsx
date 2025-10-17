f342fe8091f2264b052cd16feb10770f0f0dab53
Kathleen Moriarty <katie.moriarty13@gmail.com>
2025-10-17 13:59:52 -0700

    Add SearchBar functionality (search, setSearch)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React, { useState } from "react"
+import SearchBar from "./searchBar/SearchBar";
 
 function App() {
 
+  const [search, setSearch] = useState("");
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <main>
+      <nav>
+        <div>
+          <img src="../src/assets/reddit.png" alt="logo"/>
+          <h1>diet reddit</h1>
+        </div>
+        <SearchBar search={search} setSearch={setSearch}/>
+        <a href="http://www.reddit.com" target="_blank"><button>Go to reddit</button></a>
+      </nav>
+      
+    </main>
   )
 }
 
---
+++ src/searchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SearchBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+const SearchBar = ({ search, setSearch }) => {
+  return (
+    <div>
+      <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search Reddit"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
